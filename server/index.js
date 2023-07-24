b06c4e74585783da95f9c1bb5476eb19a4058304
Victor Oldensand <vmold@kth.se>
2023-07-24 14:03:46 +0200

    Chore: set up dependencies and main components of index.js in the server

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url); // stores the path to this index.js file
+const __dirname = path.dirname(__filename); // stores the path to this directory
+dotenv.config(); // loads environment variables from a .env file in the project directory
+const app = express(); // creates instance of Express.js application
+app.use(express.json()); // adds middleware to parse incoming requests as json data
+app.use(helmet()); // adds middleware to secure application by setting HTTP headers
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // configures helmet to allow cross-origin requests for all resources
+app.use(morgan("common")); // adds middleware that logs info about incoming requests to server
+app.use(bodyParser.json({ limit: "30mb", extended: true })); // adds middleware that parses incoming request bodies to easier access the data sent in the request
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); // adds middleware that parses url encoded data
+app.use(cors); // adds middleware that allows you to define which origins can access your servers resources
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); // adds middleware that can serve static files from public/assets when the server receives requests with the /assets path
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // cb is a callback function
+    cb(null, "public/assets"); // null indicates no error
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001; // the second one is a backup
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
